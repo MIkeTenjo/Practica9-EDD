96af0be106913d2b39e5070d713e62d30171d37d
JoséLuisHT <joseluisht@ciencias.unam.mx>
2025-10-27 13:34:48 -0600

    actualizarUltimoAgregado

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -1,17 +1,38 @@
 import java.util.Iterator;
 
+/**
+ * Ckase abstracta que modela un Arbol Binario de un tipo
+ * genérico. La clase implementa la clase Coleccion lo que le
+ * permite hacer acciones como inserción o eliminación de elementos.
+ * 
+ * @param <T> el tipo de elementos almacenados en el árbol.
+ */
 public abstract class ArbolBinario<T> implements Coleccion<T> {
 
+    /**
+     * Clase interna protegida que simula un nodo del árbol (Vértice).
+     */
     protected class Vertice {
 
+        /*El elemento del vértice. */
         public T elemento;
 
+        /*El hijo izquierdo del vértice */
         public Vertice izquierdo;
 
+        /*El hijo derecho del vértice */
         public Vertice derecho;
 
+        /*El padre del vértice. */
         public Vertice padre;
 
+        /**
+         * Constructor que inicializa el vértice con un elemento.
+         * El vértice no se inicializa con un padre, hijo izquierdo
+         * o hijo derecho referenciados.
+         * 
+         * @param elemento El elemento del vértice.
+         */
         public Vertice(T elemento) {
             this.elemento = elemento;
             this.izquierdo = null;
@@ -19,14 +40,31 @@ public abstract class ArbolBinario<T> implements Coleccion<T> {
             this.padre = null;
         }
 
+        /**
+         * Método que determina si este vértice es hijo izquierdo de su padre.
+         * 
+         * @return {@code true} si  tiene padre y es su hijo izquierdo;
+         * {@code false} en otro caso.
+         */
         public boolean esHijoIzquierdo() {
             return padre != null && padre.izquierdo == this;
         }
 
+        /**
+         * Método que determina si este vértice es hijo derecho de su padre.
+         * 
+         * @return {@code true} si tiene padre y es su hijo derecho;
+         * {@code false} en otro caso.
+         */
         public boolean esHijoDerecho() {
             return padre != null && padre.derecho == this;
         }
 
+        /**
+         * Asigna un vértice como hijo izquierdo del vértice actual.
+         * 
+         * @param hijo El vértice que se asignará como hijo izquierdo
+         */
         public void agregaIzquierdo(Vertice hijo) {
             izquierdo = hijo;
             if (hijo != null){ 
@@ -34,11 +72,24 @@ public abstract class ArbolBinario<T> implements Coleccion<T> {
             }
         }
 
+        /**
+         * Asigna un vértice como hijo derecho del vértice actual.
+         * 
+         * @param hijo El vértice que se asignará como hijo derecho.
+         */
         public void agregaDerecho(Vertice hijo) {
             derecho = hijo;
             if (hijo != null) hijo.padre = this;
         }
 
+        /**
+         * Compara este vértice actual con otro para determinar 
+         * si son iguales.
+         * 
+         * @param o el Vértice con el que se desea comparar.
+         * @return {@code true} si el vértice a comparar es igual al actual;
+         * {@code false} en otro caso.
+         */
         @Override
         public boolean equals(Object o) {
             if(this == o){
@@ -57,39 +108,94 @@ public abstract class ArbolBinario<T> implements Coleccion<T> {
 
     }
 
+    /*Raíz del árbol. */
     protected Vertice raiz;
 
+    /*Número total de vértices en el árbol. */
     protected int tamanio;
 
+    /**
+     * Construcor que inicializa un árbol vacío.
+     */
     public ArbolBinario() {
         this.raiz = null;
         this.tamanio = 0;
     }
 
+    /**
+     * Método abstracto para buscar un elemento en el árbol.
+     * 
+     * @param elemento El elemento a buscar en el árbol.
+     * @param return {@code true} si el elemento a buscar está
+     * en el árbol; {@code false} en otro caso.
+     */
     @Override
     public abstract boolean buscar(T elemento);
 
+    /**
+     * Método que devuelve el elemento que está en la posición
+     * {@code indice}, dado un recorrido del árbol.
+     * 
+     * @param indice La posición del elemento dado un recorrido del árbol.
+     * @return El elemento correspondiente en el {@code indice}.
+     */
     public T acceder(int indice) {
         ListaDoblementeLigada<T> recorrido = devolverRecorrido();
         return recorrido.acceder(indice);
     }
 
+    /**
+     * Método que devuelve el número de vértices en el árbol.
+     * @return El número de vértices en el árbol.
+     */
     public int devolverTamanio() {
         return tamanio;
     }
 
+    /**
+     * Método que nos devuelve un valor booleano que nos dice
+     * si el árbol está vacío o no.
+     * 
+     * @return {@code true} Si el árbol está vacío. {@code false}
+     * en otro caso.
+     */
     public boolean estaVacio() {
         return raiz == null && tamanio == 0;
     }
 
+    /**
+     * Método abstracto que inserta un nuevo elemento
+     * en el árbol.
+     * 
+     * 
+     * @param elemento El elemento a insertar en el árbol.
+     */
     @Override
     public abstract void insertar(T elemento);
 
+    /**
+     * Método abstracto que elimina un elemento contenido
+     * en el árbol.
+     * 
+     * @param elemento El elemento a eliminar del árbol.
+     */
     @Override
     public abstract void eliminar(T elemento);
 
+    /**
+     * Método que devuelve una lista con los elementos del árbol
+     * siguiendo un recorrido especifico.
+     * 
+     * @return Una lista doblemente ligada con los vértices siguiendo
+     * el recorrido.
+     */
     protected abstract ListaDoblementeLigada<T> devolverRecorrido();
 
+    /**
+     * Método que permite recorrer el árbol con un iterador.
+     * 
+     * @return Un iterador que recorre el árbol según su recorrido.
+     */
     @Override
     public Iterator<T> iterator() {
         return devolverRecorrido().iterator();
--- ArbolBinarioCompleto.java
+++ ArbolBinarioCompleto.java
@@ -1,18 +1,48 @@
 import java.util.NoSuchElementException;
 
+/**
+ * Clase que simula un Árbol Binario Completo.
+ * 
+ * Un Árbol Binario Completo tiene la caracteristica en el que 
+ * todos sus niveles están llenos excpeto posiblemente el último, además
+ * todos sus vértices están acomodados de izquierda a derecha, de modo
+ * que si el último nivel tiene un "hoyo", este hoyo siempre estará
+ * del lado derecho.
+ * 
+ * @param <T> El tipo de los elementos almacenados en el árbol.
+ */
 public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
     
+    /**El último vértice agregado del árbol. */
     private Vertice ultimoAgregado;
 
+    /**
+     * Constructor que inicializa un árbol binario completo vacío.
+     */
     public ArbolBinarioCompleto() {
         super();
         ultimoAgregado = null;
     }
 
+    /**
+     * Método auxiliar que determina si un vértice es la ráiz del árbol.
+     * 
+     * @param vertice El vértice a verificar si es la raíz del árbol.
+     * @return {@code true} si {@code vertice} es la ráiz del árbol. {@code false}
+     * en otro caso.
+     */
     private boolean esRaiz(Vertice vertice){
         return vertice.padre == null && raiz.equals(vertice);
     }
 
+    /**
+     * Inserta un nuevo elemento en el árbol manteniendo la propiedad de 
+     * ser completo.
+     * 
+     * @param elemento El elemento a insertar en el árbol en el último nivel,
+     * lo más a lo izquierda posible.
+     * @throws IllegalArgumetnException Si el elemento es nulo.
+     */
     @Override
     public void insertar(T elemento) {
         if (elemento == null) {
@@ -37,6 +67,15 @@ public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
         tamanio++;
     }
 
+    /**
+     * Método que elimina un elemento del árbol manteniendo la propiedad
+     * de ser completo.
+     * 
+     * @param elemento El elemento a eliminar del árbol.
+     * @param IllegalArgumentException Si el elemento a querer eliminar
+     * es nulo.
+     * @param NoSuchElementException Si el árbol está vacío.
+     */
     @Override
     public void eliminar(T elemento) {
         if (elemento == null) {
@@ -45,8 +84,8 @@ public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
         if (estaVacio()) {
             throw new NoSuchElementException("El árbol está vacío.");
         }
-        Vertice eliminar;
-        Vertice ultimoVertice;
+        Vertice eliminar = null;
+        Vertice ultimoVertice = null;
         Cola<Vertice> cola = new Cola<Vertice>();
         cola.meter(raiz);
         while (!cola.estaVacia()) {
@@ -80,8 +119,31 @@ public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
         actualizarUltimoAgregado();
     }
 
+    /**
+     * Actualiza el valor del último vértice agregado en el árbol.
+     */
     private void actualizarUltimoAgregado() {
-        /*Aqui va tu codigo*/
+        if (estaVacio()) {
+            ultimoAgregado = null;
+            return;
+        }
+
+        Cola<Vertice> cola = new Cola<>();
+        cola.meter(raiz);
+        Vertice ultimo = null;
+
+        while (!cola.estaVacia()) {
+            ultimo = cola.sacar();
+
+            if (ultimo.izquierdo != null) {
+                cola.meter(ultimo.izquierdo);
+            }
+            if (ultimo.derecho != null) {
+                cola.meter(ultimo.derecho);
+            }
+        }
+
+        ultimoAgregado = ultimo;
     }
 
 
